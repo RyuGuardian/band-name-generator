d00b9110206cbb905dac7c01370a14fdcc8e4bd4
Austin King <aust.a.king@gmail.com>
2015-07-29 15:32:29 -0700

    Can get full random band name and submit adjectives.

--- app/script.js
+++ app/script.js
@@ -1,11 +1,47 @@
 'use strict'
 
-$(function() {
+$(document).ready(function() {
 
 	$("#name").click(function() {
 		$.get("adjective", function(response) {
 			var adj = response.word;
-			$("#adjective").text(adj);
+			$("#band_name").text(adj);
 		});
+		$.get("noun", function(response) {
+			var noun = response.word;
+			$("#band_name").append(" " + noun);
+		});
+		$.get("verb", function(response) {
+			$("#band_name").append(" " + response.word);
+		});
+	});
+
+	// 	$("#name").click(function() {		
+	// 	$.get("band_name", function(response) {
+	// 		var bandName = response.word;
+
+	// 		$("#band_name").text(bandName);
+	// 	});
+	// });
+
+	//make event handler that sends POST request to our server
+	//when submit button is pressed
+
+	$("#submitWords").on("submit", function(e) {
+		e.preventDefault();
+
+		//get the text in the text box and save to variable
+		var adj = $("input[name=adj]").val();
+		var adjPost;
+
+		if(adj) {
+			adjPost = {word: adj};
+			$.post('adjective', adjPost, function(response) {
+				console.log(response);
+				var adjResponse = response.message;
+				$("#adjResponse").text(adjResponse);
+			})
+		}
+
 	});
 });
--- server.js
+++ server.js
@@ -1,35 +1,46 @@
 'use strict'
 
 var express = require("express");
+var bodyparser = require("body-parser");
+var Adjectives = require("./lib/adjectives.js");
+var Nouns = require("./lib/nouns.js");
+var Verbs = require("./lib/verbs.js");
+var getRandomWord = require("./lib/getRandomWord.js");
+var postAddWord = require("./lib/postAddWord.js");
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/app/"));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 
-function Adj() {
-	this.tired = true;
-	this.unimpressed = true;
-	this.soulCrushing = true;
-	this.lame = true;
-	this.eerie = true;
-};
-
-var adj = new Adj();
-
-function getRandomWord(obj) {
-	var propArr = Object.keys(obj);
-
-	var randomProp = propArr[Math.floor(Math.random() * propArr.length)];
-
-	return {word: randomProp};
-}
+var adjectives = new Adjectives();
+var nouns = new Nouns();
+var verbs = new Verbs();
 
 app.get("/", function(req, res) {
 	res.sendFile("index.html");
 });
 
 app.get("/adjective", function(req, res) {
-	res.json(getRandomWord(adj));
+	res.json(getRandomWord(adjectives));
+});
+
+app.get("/noun", function(req, res) {
+	res.json(getRandomWord(nouns));
+});
+
+app.get("/verb", function(req, res) {
+	res.json(getRandomWord(verbs));
+});
+
+// app.get("/band_name", function(req, res) {
+// 	res.json(getRandomWord(adjectives, nouns, verbs));
+// });
+
+app.post("/adjective", function(req, res) {
+	console.log(req.body);
+	res.json(postAddWord(req.body.word, adjectives));
 });
 
 app.listen(port, function() {
